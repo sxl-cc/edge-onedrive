5df6eca0bad0d5b61f707d9a3d9f871895772328
gmero <gmerocom@gmail.com>
2026-05-20 13:50:19 +0800

    chore: log public

--- examples/vercel/scripts/copy-ui.mjs
+++ examples/vercel/scripts/copy-ui.mjs
@@ -7,6 +7,10 @@ const projectRoot = resolve(currentDir, "..");
 const uiDistDir = resolve(projectRoot, "../../ui/dist");
 const publicDir = resolve(projectRoot, "public");
 
+console.log(`Copying ${uiDistDir} to ${publicDir}`);
+
 await rm(publicDir, { force: true, recursive: true });
 await mkdir(publicDir, { recursive: true });
 await cp(uiDistDir, publicDir, { recursive: true });
+
+console.log("Done");
